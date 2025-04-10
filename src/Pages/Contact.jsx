219b6ee84e2c47bebce6640362690e0d7aa26985
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-04-11 00:37:26 +0530

    added products

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ContactForm from '../Components/ContactForm'
+import Navbar from '../Components/Navbar'
 const Contact = () => {
   return (
     <>
+    <Navbar/>
     <ContactForm/>
     </>
   )
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Products } from '../Components/Products'
+import Navbar from '../Components/Navbar';
+
 const  ProductPage = () => {
   return (
     <>
+    <Navbar/>
       <Products/>
     </>
   )
