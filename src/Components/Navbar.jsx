3a73b0201f0e6d9f917b6e100335b34734c119d0
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-04-11 23:37:29 +0530

    added redux

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,22 +7,24 @@ import { useSelector } from 'react-redux';
 const Navbar = ({ setIsCartOpen }) => {
   const [open, setOpen] = React.useState(false);
 
-  // Get cart items from Redux
+
   const cartItems = useSelector((state) => state.cart.cartItems);
   const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <nav className="sticky top-0 z-60 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+      
       <NavLink to='/'>
         <img className="h-9" src={logo} alt="dummyLogoColored" />
       </NavLink>
 
-      {/* Desktop Menu */}
+     
       <div className="hidden sm:flex items-center gap-8">
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/products'>All Products</NavLink>
         <NavLink to='/contact'>Contact</NavLink>
 
+      
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
@@ -35,6 +37,7 @@ const Navbar = ({ setIsCartOpen }) => {
           </svg>
         </div>
 
+     
         <div className="relative cursor-pointer" onClick={() => setIsCartOpen(true)}>
           <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" strokeLinecap="round" strokeLinejoin="round" />
@@ -46,6 +49,7 @@ const Navbar = ({ setIsCartOpen }) => {
           )}
         </div>
 
+     
         <header className="flex justify-end p-4 gap-4 cursor-pointer px-8 py-2 bg-green-500 hover:bg-green-600 transition text-white rounded-full">
           <SignedOut>
             <SignInButton mode="modal" className="cursor-pointer" />
@@ -56,7 +60,7 @@ const Navbar = ({ setIsCartOpen }) => {
         </header>
       </div>
 
-      {/* Hamburger Menu Button */}
+     
       <button onClick={() => setOpen(!open)} aria-label="Menu" className="sm:hidden">
         <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect width="21" height="1.5" rx=".75" fill="#426287" />
@@ -65,12 +69,29 @@ const Navbar = ({ setIsCartOpen }) => {
         </svg>
       </button>
 
-      {/* Mobile Menu */}
-      <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-50`}>
+    
+      <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-4 px-5 text-sm md:hidden z-50`}>
         <NavLink to='/' onClick={() => setOpen(false)}>Home</NavLink>
         <NavLink to='/products' onClick={() => setOpen(false)}>All Products</NavLink>
         <NavLink to='/contact' onClick={() => setOpen(false)}>Contact</NavLink>
-        <header className="flex justify-end p-4 gap-4 cursor-pointer px-8 py-2 bg-green-500 hover:bg-green-600 transition text-white rounded-full">
+
+      
+        <div className="relative cursor-pointer" onClick={() => {
+          setIsCartOpen(true);
+          setOpen(false);
+        }}>
+          <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          {cartCount > 0 && (
+            <button className="absolute -top-2 -right-3 text-xs text-white bg-green-500 w-[18px] h-[18px] rounded-full">
+              {cartCount}
+            </button>
+          )}
+        </div>
+
+       
+        <header className="flex justify-end gap-4 cursor-pointer px-8 py-2 bg-green-500 hover:bg-green-600 transition text-white rounded-full mt-2">
           <SignedOut>
             <SignInButton mode="modal" />
           </SignedOut>
